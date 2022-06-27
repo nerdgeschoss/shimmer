40267fb994e905ba505cc9f47b898b3fd3422c74
Jens Ravens <jens@nerdgeschoss.de>
2022-06-27 11:56:15 +0200

    meta helpers

---
+++ lib/shimmer/utils/meta.rb
@@ -0,0 +1,27 @@
+module Shimmer
+  class Meta
+    attr_accessor :title, :description, :image, :canonical
+
+    def tags
+      tags = []
+      title = self.title.present? ? "#{self.title} | eaze" : "eaze"
+      tags.push(type: :title, value: title)
+      tags.push(property: "og:title", content: title)
+      if description.present?
+        tags.push(name: :description, content: description)
+        tags.push(property: "og:description", content: description)
+      end
+      if image.present?
+        tags.push(property: "og:image", content: image)
+      end
+      if canonical.present?
+        tags.push(type: :link, rel: :canonical, href: canonical)
+        tags.push(property: "og:url", content: canonical)
+      end
+      tags.push(property: "og:type", content: :website)
+      tags.push(property: "og:locale", content: I18n.locale)
+      tags.push(name: "twitter:card", content: :summary_large_image)
+      tags
+    end
+  end
+end
---
+++ lib/shimmer/utils/meta_helper.rb
@@ -0,0 +1,30 @@
+module Shimmer
+  module MetaHelper
+    def meta
+      @meta ||= Meta.new.tap do |meta|
+        meta.canonical = url_for(only_path: false)
+      end
+    end
+
+    def title(value)
+      meta.title = value
+    end
+
+    def description(value)
+      meta.description = value
+    end
+
+    def image(value)
+      meta.image = image_file_url(value, width: 1200)
+    end
+
+    def render_meta
+      tags = meta.tags.map do |tag|
+        type = tag.delete(:type) || "meta"
+        value = tag.delete(:value)
+        content_tag(type, value, tag)
+      end
+      safe_join tags, "\n"
+    end
+  end
+end
