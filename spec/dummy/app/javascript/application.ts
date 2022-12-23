cd32f6f6295b1206e2433764cc04a71365001d6d
Adeynack <Adeynack@users.noreply.github.com>
2022-12-23 15:37:37 -0500

    re-create dummy Rails app

--- spec/dummy/app/javascript/application.ts
+++ spec/dummy/app/javascript/application.ts
@@ -4,4 +4,7 @@ import { start } from "./../../../../src/index";
 import { application } from "./controllers/application";
 import "./controllers";
 
+application.debug = false;
+window.Stimulus = application;
+
 start({ application });
