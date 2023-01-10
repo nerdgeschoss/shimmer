de3dc0f61933a5410294ffd34c4dd42561b7ae7b
Adeynack <Adeynack@users.noreply.github.com>
2023-01-10 09:23:10 -0500

    fix modal test

--- spec/dummy/spec/system/modal_spec.rb
+++ spec/dummy/spec/system/modal_spec.rb
@@ -9,15 +9,17 @@ RSpec.describe "Modal" do
     visit posts_path
     expect(page).to have_content "Posts"
     expect(page).to have_link "Write New Post"
+    expect(page).not_to have_selector ".modal--open"
+    expect(page).not_to have_content "Compose Post"
 
     click_on "Write New Post"
 
     expect(page).to have_selector ".modal--open"
-    expect(page).to have_content "New Post"
+    expect(page).to have_content "Compose Post"
     expect(page).to have_link "Cancel"
 
     click_on "Cancel"
     expect(page).not_to have_selector ".modal--open"
-    # expect(page).not_to have_content "New Post"
+    expect(page).not_to have_content "Compose Post"
   end
 end
