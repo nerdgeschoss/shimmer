f570845d0390b281a70975333e424dc4b7a566d4
Adeynack <Adeynack@users.noreply.github.com>
2023-02-09 17:14:03 +0100

    remove duplicate lines

--- spec/rails_app/app/javascript/application.js
+++ spec/rails_app/app/javascript/application.js
@@ -3,7 +3,4 @@ import { start } from "../../../../src/index";
 import { application } from "./controllers/application";
 import "./controllers";
 
-application.debug = false;
-window.Stimulus = application;
-
 start({ application });
