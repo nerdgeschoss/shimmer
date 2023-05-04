240d73a65dcdbabb89fb234d730382edbf64afd7
Justin Marsh <jmarsh24@gmail.com>
2023-05-04 15:25:13 +0200

    initial commit for secret_key_base_dummy

--- lib/shimmer/utils/config.rb
+++ lib/shimmer/utils/config.rb
@@ -1,18 +1,24 @@
-# frozen_string_literal: true
-
 module Shimmer
   class Config
     include Singleton
+
     class MissingConfigError < StandardError; end
 
-    def method_missing(method_name)
+    def method_missing(method_name, **options)
+      return nil if ENV["SECRET_KEY_BASE_DUMMY"]
+
+      default_provided = options.key?(:default)
+      default_value = options.delete(:default) if default_provided
+      raise ArgumentError, "unknown option#{"s" if options.length > 1}: #{options.keys.join(", ")}." if options.any?
+
       method_name = method_name.to_s
       type = :string
       key = method_name.delete_suffix("!").delete_suffix("?")
       required = method_name.end_with?("!")
       type = :bool if method_name.end_with?("?")
       value = ENV[key.upcase].presence
-      value ||= Rails.application.credentials.send(key)
+      value ||= Rails.application.credentials.public_send(key)
+      value = default_value if value.nil?
       raise MissingConfigError, "#{key.upcase} environment value is missing" if required && value.blank?
 
       coerce value, type
@@ -25,7 +31,7 @@ module Shimmer
     private
 
     def coerce(value, type)
-      return !value.in?(["n", "0", "no", "false"]) if type == :bool && value.is_a?(String)
+      return !value.downcase.in?(["n", "0", "no", "false"]) if type == :bool && value.is_a?(String)
 
       value
     end
