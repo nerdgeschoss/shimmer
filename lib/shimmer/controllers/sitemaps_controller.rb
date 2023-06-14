787fac5ea536847daedf5d15d07f490e0a180b1a
Justin Marsh <jmarsh24@gmail.com>
2023-06-14 14:47:49 +0200

    Update send_data with filename

--- lib/shimmer/controllers/sitemaps_controller.rb
+++ lib/shimmer/controllers/sitemaps_controller.rb
@@ -4,7 +4,9 @@ module Shimmer
   class SitemapsController < ActionController::Base
     def show
       path = "sitemaps/#{params.require(:path)}.gz"
-      send_data ActiveStorage::Blob.service.download(path)
+      filename = "sitemap_#{params[:path]}.xml.gz"
+
+      send_data ActiveStorage::Blob.service.download(path), filename:, type: "application/gzip", disposition: "attachment"
     end
   end
 end
