bf8938eec833ba4e01e9ff1da12579570f805abc
Justin Marsh <jmarsh24@gmail.com>
2023-10-25 09:51:07 +0200

    add_google_tag_manager (#83)
    * add tracking and corresponding test
    
    * initial commit for add_google_tag_manager
    
    * fix tracking spec
    
    * does not embed
    
    * rename spec
    
    * use consentfor
    
    * working scripts
    
    * update spec name
    
    * rewrite tests
    
    * refactor consent
    
    * fix consent.ts
    
    * remove new line
    
    * remove .env
    
    * update readme
    
    * add readme
    
    * adjust readme
    
    * Update README.md
    
    Co-authored-by: Daniel Diekmeier <fax@danieldiekmeier.de>
    
    * switch language to typescript
    
    * add comment
    
    ---------
    
    Co-authored-by: Daniel Diekmeier <fax@danieldiekmeier.de>

--- src/consent.ts
+++ src/consent.ts
@@ -1,7 +1,7 @@
 import { getCookie, setCookie } from "./util";
 
 const consentCategories = ["essential", "targeting", "statistic"] as const;
-export type ConsentCategory = typeof consentCategories[number];
+export type ConsentCategory = (typeof consentCategories)[number];
 
 export class Consent {
   #consentListeners: Record<ConsentCategory, Array<() => void>> = {
@@ -62,5 +62,39 @@ export class Consent {
       `https://www.googletagmanager.com/gtag/js?id=${id}`
     );
     document.head.prepend(script);
+  }
+
+  async enableGoogleTagManager(
+    id: string,
+    role: ConsentCategory = "statistic"
+  ): Promise<void> {
+    await this.consentFor(role);
+    window.gtag({
+      "gtm.start": new Date(),
+      event: "gtm.js",
+    });
+    window.gtag("config", id, { send_page_view: false }); // page view is disabled because it's tracked via the analytics stimulus controller already
+    const script = document.createElement("script");
+    script.async = true;
+    script.setAttribute(
+      "src",
+      `https://www.googletagmanager.com/gtm.js?id=${id}`
+    );
+    document.head.prepend(script);
+
+    // The following 'noscript' tag inclusion is primarily for compliance
+    // with Google's recommended installation procedure, its
+    // not technically necessary for functionality.
+    const noscript = document.createElement("noscript");
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute(
+      "src",
+      `https://www.googletagmanager.com/ns.html?id=${id}`
+    );
+    iframe.setAttribute("height", "0");
+    iframe.setAttribute("width", "0");
+    iframe.setAttribute("style", "display:none;visibility:hidden");
+    noscript.appendChild(iframe);
+    document.body.prepend(noscript);
   }
 }
