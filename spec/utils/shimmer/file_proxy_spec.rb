2a31378013c827324bb5f834d0f9480091310b06
Daniel Diekmeier <daniel@nerdgeschoss.de>
2024-05-28 17:09:00 +0200

    Make sure not do upscale

---
+++ spec/utils/shimmer/file_proxy_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Shimmer::FileProxy do
+  let(:blob) {
+    ActiveStorage::Blob.create_and_upload!(
+      io: File.open(Rails.root.join("spec/fixtures/files/nerdgeschoss.jpg")),
+      filename: "example",
+      content_type: "image/jpeg"
+    )
+  }
+
+  describe "#variant" do
+    it "returns the blob if no resize requested" do
+      variant = Shimmer::FileProxy.new(blob_id: blob.id).variant
+
+      expect(variant).to eq(blob)
+    end
+
+    context "when requesting different size" do
+      it "scales the image down" do
+        variant = Shimmer::FileProxy.new(blob_id: blob.id, width: 100, height: 200).variant
+
+        expect(dimensions(variant.processed.image.blob)).to eq({width: 100, height: 35})
+      end
+
+      it "does not scale the image up" do
+        variant = Shimmer::FileProxy.new(blob_id: blob.id, width: 1000).variant
+
+        original_dimensions = dimensions(blob)
+        new_dimensions = dimensions(variant.processed.image.blob)
+
+        expect(new_dimensions).to eq(original_dimensions)
+      end
+    end
+  end
+
+  def dimensions(blob)
+    ActiveStorage::Analyzer::ImageAnalyzer::ImageMagick.new(blob).metadata
+  end
+end
