2f24f8b116d87076a19bc01ee7611b0eec925991
Daniel Diekmeier <daniel@nerdgeschoss.de>
2024-07-19 18:03:21 +0200

    Improve sample app

--- spec/rails_app/db/seeds.rb
+++ spec/rails_app/db/seeds.rb
@@ -6,3 +6,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.find_each do |post|
+  post.image.attach(
+    io: File.open("#{Rails.root}/spec/fixtures/files/nerdgeschoss.jpg"),
+    filename: "nerdgeschoss.jpg",
+    content_type: "image/jpeg"
+  )
+end
