7a801c6b5e9c81fd9f2d92640851008677f80603
Jens Ravens <jens@nerdgeschoss.de>
2024-09-23 14:59:52 +0200

    lint

--- lib/shimmer/controllers/sitemaps_controller.rb
+++ lib/shimmer/controllers/sitemaps_controller.rb
@@ -3,7 +3,7 @@
 module Shimmer
   class SitemapsController < ActionController::Base
     def show
-      path = request.path[1..-1]
+      path = request.path[1..]
       filename = path.gsub("/", "-")
 
       send_data ActiveStorage::Blob.service.download(path), filename: filename, type: "application/gzip", disposition: "attachment"
