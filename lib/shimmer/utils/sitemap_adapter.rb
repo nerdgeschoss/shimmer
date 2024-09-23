f460de12f7031d1a3500c0e9c2afb6cfd513fcbf
Jens Ravens <jens@nerdgeschoss.de>
2024-09-23 14:55:07 +0200

    dynamic paths in storage service

--- lib/shimmer/utils/sitemap_adapter.rb
+++ lib/shimmer/utils/sitemap_adapter.rb
@@ -4,7 +4,7 @@ module Shimmer
   class SitemapAdapter
     def write(location, raw_data)
       SitemapGenerator::FileAdapter.new.write(location, raw_data)
-      ActiveStorage::Blob.service.upload("sitemaps/#{location.path_in_public}", File.open(location.path))
+      ActiveStorage::Blob.service.upload(location.path_in_public, File.open(location.path))
     end
   end
 end
