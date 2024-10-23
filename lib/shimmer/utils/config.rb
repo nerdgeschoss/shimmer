250ea633de1c9b13187dc76c02462963ce70ffb4
Jens Ravens <jens@nerdgeschoss.de>
2024-10-23 14:59:45 +0200

    Config Override and Stubbing (#100)
    * initial commit for Config Override and Stubbing
    
    * upgrade ci node
    
    * allow overriding configs with a block

--- lib/shimmer/utils/config.rb
+++ lib/shimmer/utils/config.rb
@@ -17,8 +17,9 @@ module Shimmer
       key = method_name.delete_suffix("!").delete_suffix("?").to_sym
       required = method_name.end_with?("!")
       type = :bool if method_name.end_with?("?")
-      value = ENV[key.to_s.upcase].presence
-      value ||= credentials.dig(Rails.env.to_sym, key)
+      value = @stubbed_values&.dig(key)
+      value ||= ENV[key.to_s.upcase].presence
+      value ||= credentials.dig(ENV["CONFIG_ENV"]&.to_sym || Rails.env.to_sym, key)
       value ||= credentials[key]
       value = default_value if value.nil?
       raise MissingConfigError, "#{key.upcase} environment value is missing" if required && value.blank?
@@ -28,6 +29,13 @@ module Shimmer
 
     def respond_to_missing?(method_name, include_all)
       true
+    end
+
+    def stub(**values)
+      @stubbed_values = values.to_h.deep_transform_keys(&:to_sym)
+      yield
+    ensure
+      @stubbed_values = nil
     end
 
     private
